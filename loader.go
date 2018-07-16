91f6d85d195dca13e3c328f96e3296730f2bd85f
Alex Whitney <alex@itshalfempty.com>
2018-07-16 11:19:28 -0400

    quick and dirty environment variable overrides

--- loader.go
+++ loader.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"io/ioutil"
+	"os"
 	"regexp"
 	"strings"
 
@@ -13,18 +14,60 @@ type Options struct {
 	Directory   string
 }
 
-func readFile(file string, out interface{}) error {
+func applyFile(file string, out interface{}) error {
 	fileContents, err := ioutil.ReadFile(file)
 	if err != nil {
 		return err
 	}
 
-	err = yaml.Unmarshal(fileContents, out)
+	return apply(fileContents, out)
+}
+
+func applyEnvVars(file string, out interface{}) error {
+	fileContents, err := ioutil.ReadFile(file)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	// Read the yaml file. Assume every value is a string that indicates the
+	// env var that should be used for the corresponding key.
+
+	// This is hacky, and likely only supports a subset of yaml syntax
+	// I couldn't find a parsing package after a pretty quick search,
+	// and this is good enough for my needs for now.
+
+	withReplacements := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+	for _, line := range lines {
+		envVar := ""
+		prefix := ""
+		if strings.Contains(line, ":") {
+			rexp := regexp.MustCompile("^(.*):\\s+((?:\\\".*\\\"\\s+)|(?:.*))$")
+			matches := rexp.FindStringSubmatch(line)
+			if len(matches) > 0 {
+				prefix = matches[1]
+				envVar = strings.Trim(matches[2], "\" \t")
+			}
+		}
+
+		if envVar != "" {
+			val := os.Getenv(envVar)
+			if val != "" {
+				line = prefix + ": " + val
+			} else {
+				line = ""
+			}
+		}
+
+		withReplacements = withReplacements + "\n" + line
+	}
+
+	return apply([]byte(withReplacements), out)
+}
+
+func apply(yamlBuf []byte, out interface{}) error {
+	return yaml.Unmarshal(yamlBuf, out)
 }
 
 func generateFilePrefixes(deployment string) []string {
@@ -70,10 +113,18 @@ func Load(opts *Options, out interface{}) error {
 	for _, prefix := range loadOrder {
 		fileName, exists := fileHash[prefix]
 		if exists {
-			err := readFile(directory+"/"+fileName, out)
+			err := applyFile(directory+"/"+fileName, out)
 			if err != nil {
 				return err
 			}
+		}
+	}
+
+	fileName, exists := fileHash["custom-environment-variables."]
+	if exists {
+		err := applyEnvVars(directory+"/"+fileName, out)
+		if err != nil {
+			return err
 		}
 	}
 
--- loader_test.go
+++ loader_test.go
@@ -1,9 +1,10 @@
 package config
 
 import (
+	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadConfig(t *testing.T) {
@@ -15,43 +16,87 @@ func TestLoadConfig(t *testing.T) {
 		E string
 	}
 
-	cases := []struct {
+	cases := map[string]struct {
 		directory string
 		env       string
 		envVar    map[string]string
 		host      string
 		input     interface{}
 		expect    interface{}
-	}{{
-		directory: "simple",
-		input:     &simpleConfig{},
-		expect: &simpleConfig{
-			A: "foo",
-			B: true,
-			C: 123.34,
-			D: 111,
-			E: "default",
+	}{
+		"defaults": {
+			directory: "simple",
+			input:     &simpleConfig{},
+			expect: &simpleConfig{
+				A: "foo",
+				B: true,
+				C: 123.34,
+				D: 111,
+				E: "default",
+			},
 		},
-	}, {
-		directory: "simple",
-		env:       "production",
-		input:     &simpleConfig{},
-		expect: &simpleConfig{
-			A: "foo",
-			B: true,
-			C: 123.34,
-			D: 111,
-			E: "production",
+		"env overrides": {
+			directory: "simple",
+			env:       "production",
+			input:     &simpleConfig{},
+			expect: &simpleConfig{
+				A: "foo",
+				B: true,
+				C: 123.34,
+				D: 111,
+				E: "production",
+			},
 		},
-	}}
+		"local & env overrides": {
+			directory: "local",
+			env:       "production",
+			input:     &simpleConfig{},
+			expect: &simpleConfig{
+				A: "newer",
+				C: 22,
+				D: 33,
+				E: "local-prod",
+			},
+		},
+		"env var overrides": {
+			directory: "env-var",
+			input:     &simpleConfig{},
+			envVar: map[string]string{
+				"ENV_C": "55.555",
+			},
+			expect: &simpleConfig{
+				A: "foo",
+				C: 55.555,
+			},
+		},
+		"missing env var overrides": {
+			directory: "env-var",
+			input:     &simpleConfig{},
+			expect: &simpleConfig{
+				A: "foo",
+				C: 123.34,
+			},
+		},
+	}
 
-	for _, test := range cases {
+	for name, test := range cases {
 		opts := &Options{
 			Environment: test.env,
 			Directory:   "./testConfigs/" + test.directory,
 		}
+
+		originalEnv := map[string]string{}
+		for k, v := range test.envVar {
+			originalEnv[k] = os.Getenv(k)
+			os.Setenv(k, v)
+		}
+
 		err := Load(opts, test.input)
-		require.Nil(t, err)
-		require.Equal(t, test.expect, test.input)
+		assert.Nil(t, err, name)
+		assert.Equal(t, test.expect, test.input, name)
+
+		for k, v := range originalEnv {
+			os.Setenv(k, v)
+		}
 	}
 }
